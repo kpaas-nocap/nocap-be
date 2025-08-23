bc5b33c882c1e36763fc8e0afd7a4bb3ec35f820
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-08-23 16:51:24 +0900

    Refactor : NewsSearchRequestDto 수정
    제목 외에도 url이 같을 때 제외할 수 있도록 String excludeUrl을 필드로 추가

--- src/main/java/com/example/nocap/domain/analysis/dto/NewsSearchRequestDto.java
+++ src/main/java/com/example/nocap/domain/analysis/dto/NewsSearchRequestDto.java
@@ -10,5 +10,6 @@ public class NewsSearchRequestDto {
     // Naver News API를 요청할 키워드와 결과에서 제외할 제목(원본 제목)
     private String rawKeyword;
     private String excludeTitle;
+    private String excludeUrl;
 
 }
