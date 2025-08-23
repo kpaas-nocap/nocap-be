9a4e64584cabd906e503e241ce64498ab5f3914a
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-08-23 16:54:18 +0900

    Feat : AnalysisRepository 생성
    Analysis 저장을 위한 repository 생성

--- src/main/java/com/example/nocap/domain/analysis/repository/AnalysisRepository.java
+++ src/main/java/com/example/nocap/domain/analysis/repository/AnalysisRepository.java
@@ -1,5 +1,8 @@
 package com.example.nocap.domain.analysis.repository;
 
-public interface AnalysisRepository{
+import com.example.nocap.domain.analysis.entity.Analysis;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnalysisRepository extends JpaRepository<Analysis, Long> {
 
 }
