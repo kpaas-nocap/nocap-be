32824572d8c26944e3274b7fae0e0be0a8e43112
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-09-07 22:00:51 +0900

    Refactor : 중간테이블 추가에 따른 수정
    Analysis 저장에 있어서 중간 테이블에도 정보가 저장되도록 수정

--- src/main/java/com/example/nocap/domain/analysis/repository/AnalysisRepository.java
+++ src/main/java/com/example/nocap/domain/analysis/repository/AnalysisRepository.java
@@ -1,14 +1,20 @@
 package com.example.nocap.domain.analysis.repository;
 
 import com.example.nocap.domain.analysis.entity.Analysis;
+import com.example.nocap.domain.mainnews.entity.MainNews;
 import com.example.nocap.domain.user.entity.User;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AnalysisRepository extends JpaRepository<Analysis, Long> {
 
     List<Analysis> findAllByCategory(String category);
 
-    List<Analysis> findAllByUserId(Long id);
+    @Query("SELECT a FROM Analysis a JOIN a.userAnalyses ua WHERE ua.user.id = :userId")
+    List<Analysis> findAllByUserId(@Param("userId") Long userId);
+
+    Optional<Analysis> findByMainNews(MainNews mainNews);
 }
