f730658ec94fdc540c704e759e96784b30718d90
dlqja <beomjin754@gmail.com>
2025-09-19 16:09:13 +0900

    [Fix] SwaggerConfig

---
+++ src/main/java/com/example/nocap/global/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.example.nocap.global;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Components comps = new Components()
+                .addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"));
+        return new OpenAPI().components(comps);
+    }
+}
