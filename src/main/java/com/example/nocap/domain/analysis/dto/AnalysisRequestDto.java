09f79a26913a204242d99b760548dae8f922484e
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-09-30 17:41:23 +0900

    Refactor: Analysis 요청 DTO 수정
    user 정보는 토큰에서 추출
    플랜과 뉴스DTO로 분석 시도

--- src/main/java/com/example/nocap/domain/analysis/dto/AnalysisRequestDto.java
+++ src/main/java/com/example/nocap/domain/analysis/dto/AnalysisRequestDto.java
@@ -1,9 +1,10 @@
 package com.example.nocap.domain.analysis.dto;
 
+import com.example.nocap.domain.searchnews.dto.SearchNewsDto;
 import lombok.Data;
 
 @Data
 public class AnalysisRequestDto {
-    private String url;
-    private Long userId;
+    private String plan;
+    private SearchNewsDto searchNewsDto;
 }
