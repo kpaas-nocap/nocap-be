14bb9c4ec16292ee2a0be07ea8269c235504f69e
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-10-01 18:09:41 +0900

    Feat: 댓글 내용, 날짜, 추천과 비추천을 반환하는 Dto 생성

---
+++ src/main/java/com/example/nocap/domain/comment/dto/CommentResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.nocap.domain.comment.dto;
+
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@Data
+public class CommentResponseDto {
+
+    private String content;
+
+    private LocalDateTime date;
+
+    private int recommendation;
+
+    private int nonRecommendation;
+
+}
