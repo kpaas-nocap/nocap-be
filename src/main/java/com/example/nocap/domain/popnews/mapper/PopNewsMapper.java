f93b3585a7d316b0162ea6a10747f8ac0ec7b3fa
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-10-04 00:47:10 +0900

    refactor: 인기뉴스에 대해 전체조회, 개별 조회 Dto구분

--- src/main/java/com/example/nocap/domain/popnews/mapper/PopNewsMapper.java
+++ src/main/java/com/example/nocap/domain/popnews/mapper/PopNewsMapper.java
@@ -2,7 +2,9 @@ package com.example.nocap.domain.popnews.mapper;
 
 import com.example.nocap.domain.mainnews.mapper.MainNewsMapper;
 import com.example.nocap.domain.news.mapper.NewsMapper;
+import com.example.nocap.domain.popnews.dto.PopNewsDetailDto;
 import com.example.nocap.domain.popnews.dto.PopNewsDto;
+import com.example.nocap.domain.popnews.dto.PopNewsSummaryDto;
 import com.example.nocap.domain.popnews.entity.PopNews;
 import java.util.List;
 import org.mapstruct.Mapper;
@@ -12,6 +14,12 @@ import org.mapstruct.Mapper;
 public interface PopNewsMapper {
 
     PopNewsDto toPopNewsDto(PopNews popNews);
+
+    PopNewsDetailDto toPopNewsDetailDto(PopNews popNews);
+    PopNewsSummaryDto toPopNewsSummaryDto(PopNews popNews);
+
+    List<PopNewsDetailDto> toPopNewsDetailDtoList(List<PopNews> popNewsList);
+    List<PopNewsSummaryDto> toPopNewsSummaryDtoList(List<PopNews> popNewsList);
 
     List<PopNewsDto> toPopNewsDtoList(List<PopNews> popNewsList);
 
