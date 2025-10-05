0b85514c4fb6b964540df6b36f9012d39fbf5a77
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-10-05 20:45:05 +0900

    refactor: Question 필드에 제목 삭제 후 이메일과 문의 분류 추가

--- src/main/java/com/example/nocap/domain/question/entity/Question.java
+++ src/main/java/com/example/nocap/domain/question/entity/Question.java
@@ -3,6 +3,8 @@ package com.example.nocap.domain.question.entity;
 import com.example.nocap.domain.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -33,13 +35,21 @@ public class Question {
     private User user;
 
     @Column(nullable = false)
-    private String title;
+    private String email;
+
+    @Enumerated(EnumType.STRING) // DB에 Enum 이름을 문자열로 저장
+    @Column(nullable = false)
+    private QuestionCategory category;
 
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    @Column(columnDefinition = "TEXT")
+    private String answer;
+    private LocalDateTime answeredAt;
 
     public void setUser(User user) {
         this.user = user;
