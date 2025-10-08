4e9e684a76ddae2e1e714e62d164c347281ca932
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-10-08 14:07:01 +0900

    refactor: JwtUtil 내부에서 토큰 정보를 반환하는 resolveToken 메서드 추가

--- src/main/java/com/example/nocap/auth/config/JwtUtil.java
+++ src/main/java/com/example/nocap/auth/config/JwtUtil.java
@@ -2,6 +2,7 @@ package com.example.nocap.auth.config;
 
 import com.example.nocap.domain.user.entity.User;
 import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -9,14 +10,22 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import org.springframework.util.StringUtils;
 
 @Component
 public class JwtUtil {
     private final SecretKey secretKey;
-
     public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
                 Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
     }
 
     public String getUsername(String token) {
