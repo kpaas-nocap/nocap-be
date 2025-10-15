0de6b01bd66c3654ff7d0b1bb80ff57dfa3b58b5
dlqja <beomjin754@gmail.com>
2025-10-15 20:51:35 +0900

    [Feat] Add id in historyMapper

--- src/main/java/com/example/nocap/domain/history/mapper/HistoryMapper.java
+++ src/main/java/com/example/nocap/domain/history/mapper/HistoryMapper.java
@@ -5,14 +5,15 @@ import com.example.nocap.domain.history.dto.HistoryRequestDto;
 import com.example.nocap.domain.history.dto.HistorySummaryDto;
 import com.example.nocap.domain.history.entity.History;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface HistoryMapper {
 
     History toHistory(HistoryRequestDto historyRequestDto);
 
+    @Mapping(target = "id", source = "id")
     HistorySummaryDto toHistorySummary(History history);
-
+    @Mapping(target = "id", source = "id")
     HistoryDetailDto toHistoryDetailDto(History history);
-
 }
